fd4a49acd71d2185565717fd0d86bbe2fbb3771b
sozoia <zfzg2232@gmail.com>
2024-01-17 22:28:53 +0300

    betty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,26 +7,26 @@
  * using the Bubble sort algorithm
  * @array: given array we will sort
  * @size: size of that array
- * 
+ *
 */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i,j;
+	size_t i, j;
 	size_t temp;
 
 	if (size < 1 || !array)
 		return;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
-				array[j] = array[j+1];
-				array[j+1] = temp;
-                print_array(array, size);
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
 	}
