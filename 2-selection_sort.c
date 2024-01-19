6b8ace1aa0f077db055d1803384d2149a2d16655
sozoia <zfzg2232@gmail.com>
2024-01-19 09:06:00 +0300

    first try

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,12 +1,32 @@
 #include "sort.h"
 
-
 /**
- * 
- * 
- * 
+ * selection_sort - sort array using selection sort algo
+ * @array: given array
+ * @size: size of the array
 */
 void selection_sort(int *array, size_t size)
 {
+    int i, j;
+    int greater, main;
+
+	if (size < 1 || !array)
+		return;
+
+    for(i = 0; i < size; i++)
+    {
+        main = array[i];
+
+        for(j = i; j < size; j++)
+        {
+            if (array[j] < main)
+            {
+                greater = main;
+                array[i] = array[j];    /*i is current index*/
+                array[j] = greater;     /*j is the moving curser*/
+                break;
+            }
+        }
+    }
     return;
 }
