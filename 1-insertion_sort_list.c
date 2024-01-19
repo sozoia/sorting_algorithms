33b01d94699a9d9aede9a270f6ee9c4e095d7ba0
Omar-algassim <omarggg2@gmail.com>
2024-01-19 02:45:20 +0200

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,32 +12,33 @@ void insertion_sort_list(listint_t **list)
 	listint_t *befor = *list;
 	listint_t *temp = malloc(sizeof(listint_t));
 
-	key = key->next;
-	if (list != NULL)
-	{
-		while (key != NULL)
-		{
-			temp = key;
-			while (befor != NULL && key->n < befor->n)
-			{
-				if (befor->prev != NULL)
-					befor->prev->next = key;
-				if (temp->next != NULL)
-					temp->next->prev = befor;
-				befor->next = key->next;
-				key->prev = befor->prev;
-				key->next = befor;
-				befor->prev = key;
+	if (list == NULL || *list == NULL || key->next == NULL)
+		return;
 
-				befor = key->prev;
-				if (key->prev == NULL)
-					*list = key;
-				print_list(*list);
-			}
-			key = temp->next;
-			if (key != NULL)
-				befor = key->prev;
+	key = key->next;
+	while (key != NULL)
+	{
+		temp = key;
+		while (befor != NULL && key->n < befor->n)
+		{
+			if (befor->prev != NULL)
+				befor->prev->next = key;
+			if (temp->next != NULL)
+				temp->next->prev = befor;
+			befor->next = key->next;
+			key->prev = befor->prev;
+			key->next = befor;
+			befor->prev = key;
+
+			befor = key->prev;
+			if (key->prev == NULL)
+				*list = key;
+			print_list(*list);
 		}
+		key = temp->next;
+		if (key != NULL)
+			befor = key->prev;
 	}
+
 }
 
