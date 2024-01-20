fb1aba3bbc08bcce4bdd140478bdc496eedc974f
sozoia <zfzg2232@gmail.com>
2024-01-20 21:53:37 +0300

    betty style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,30 +9,30 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j;
-    size_t smallest, temp;
+	size_t i, j;
+	size_t smallest, temp;
 
-    if (size < 2 || !array)
-        return;
+	if (size < 2 || !array)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        smallest = i;
+	for (i = 0; i < size - 1; i++)
+	{
+		smallest = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[smallest])
-            {
-                smallest = j;
-            }
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[smallest])
+			{
+				smallest = j;
+			}
+		}
 
-        if (smallest != i)
-        {
-            temp = array[i];
-            array[i] = array[smallest];
-            array[smallest] = temp;
-            print_array(array, size);
-        }
-    }
+		if (smallest != i)
+		{
+			temp = array[i];
+			array[i] = array[smallest];
+			array[smallest] = temp;
+			print_array(array, size);
+		}
+	}
 }
